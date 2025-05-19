2f36898857ae73677a4fe0d2b85c46f6014ba9d7
Alan Andrade <alanandrade4985@gmail.com>
2025-05-19 10:20:33 -0300

    aula de repetições
    While, do - while

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var contador = 1;
+
+do {
+    console.log(`Passo ${contador}`);
+    contador++; // c = c + 1
+} while ( contador <= 5);
+
+/*var contador = 1;
+
+while ( contador <= 5) {
+    console.log(`Passo ${contador}`);
+    contador++; // c = c + 1
+};*/
+
