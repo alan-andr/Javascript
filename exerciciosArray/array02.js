514abcba8d1b8b2578678d049ebcf45b864c63ac
Alan Andrade <alanandrade4985@gmail.com>
2025-09-04 08:47:08 -0300

    exercicios de array

---
+++ exerciciosArray/array02.js
@@ -0,0 +1,12 @@
+let nomes = ["Alan", "Gustavo", "Ana", "Pereira"];
+
+let contador = 0;
+
+for (let valor of nomes) {
+    if (valor[0] === "A") {
+        contador++;
+        console.log(`Nome que começam com a letra 'A': ${valor}`);
+    }
+}
+
+console.log(`Quantidade de nomes que começam com a letra 'A': ${contador}`);
