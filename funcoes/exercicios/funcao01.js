077a6db3e0d5d84a3c57fcdc658cb9e46e3415a1
Alan Andrade <alanandrade4985@gmail.com>
2025-09-09 11:14:35 -0300

    funções

---
+++ funcoes/exercicios/funcao01.js
@@ -0,0 +1,11 @@
+function parImpar(n) {
+    if (n % 2 === 0) {
+        return 'Par'
+    } else {
+        return 'Ímpar'
+    }
+}
+
+let resultado = parImpar(6);
+
+console.log(resultado)
---
+++ funcoes/exercicios/funcao02.js
@@ -0,0 +1,5 @@
+function somar(a = 0, b = 0) {
+    return a + b;
+}
+
+console.log(somar(10));
